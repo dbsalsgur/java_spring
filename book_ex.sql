614ba06adfda510b032011ee57dd2a42356e72b9
dbsalsgur <109213630+dbsalsgur@users.noreply.github.com>
2023-04-28 18:27:55 +0900

    230428 spring 공부

--- book_ex.sql
+++ book_ex.sql
@@ -78,3 +78,8 @@ create table tbl_sample1( col1 varchar2(500));
 
 
 create table tbl_sample2( col2 varchar2(50));
+
+alter table tbl_board add (replycnt number default 0);
+
+update tbl_board set replycnt = (select count(rno) from tbl_reply
+where tbl_reply.rno = tbl_board.bno);
